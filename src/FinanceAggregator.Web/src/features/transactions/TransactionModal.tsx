7278207daa5ed81387b03ba5cd4fba7d07397506
jumar v <jdqvillapando@users.noreply.github.com>
2026-09-09 15:59:18 +0800

    fix(frontend): create directory for form-type components

--- src/FinanceAggregator.Web/src/features/transactions/TransactionModal.tsx
+++ src/FinanceAggregator.Web/src/features/transactions/TransactionModal.tsx
@@ -5,7 +5,7 @@ import { useAppDispatch } from '../../app/store/configureStore';
 import { postNewTransaction } from '../../features/transactions/reducers/transactionSlice';
 
 import { TransactionType, type TransactionFormValues } from '../../app/models/transaction';
-import FormModal from '../../common/components/FormModal';
+import FormModal from '../../common/components/form/FormModal';
 
 
 interface Props {
