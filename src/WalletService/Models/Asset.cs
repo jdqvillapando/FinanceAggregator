f5f137b4f0e0870b4a303efc994d91cdf3d1b673
jumar v <jdqvillapando@users.noreply.github.com>
2026-09-13 18:19:36 +0800

    feat(wallet, frontend): add locale entity & implement it

--- src/WalletService/Models/Asset.cs
+++ src/WalletService/Models/Asset.cs
@@ -4,8 +4,7 @@ public class Asset
 {
     public Guid Id { get; set; }
     public string Ticker { get; set; } = string.Empty; // e.g., BTC, ETH, USD
-
-    //Financial precision is key here
     public decimal Balance { get; set; }
+    public string? Locale { get; set; }
     public Guid WalletId { get; set; }
 }
